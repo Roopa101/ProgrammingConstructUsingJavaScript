afdb3879021d9530201478d6502c83081adc44e3
Roopa Jamadar <roopajamadar14@gmail.com>
2022-01-09 14:29:22 +0530

    [Roopa] Added: Sum And Average Of 5 Random 2 Digit Using JavaScript

---
+++ SequencePracticeProblems/SumAndAverageOf5Random2Digit.js
@@ -0,0 +1,15 @@
+let Number1 = Math.floor( Math.random() * 90  +10 );
+console.log("Enter first Number :" + Number1  )
+let Number2=Math.floor(Math.random() * 90  +10 );
+console.log("Enter second Number:" +Number2 )
+let Number3 = Math.floor( Math.random() * 90  +10 );
+console.log("Enter three Number :" + Number3  )
+let Number4 = Math.floor( Math.random() * 90  +10);
+console.log("Enter fourth Number :" + Number4  )
+let Number5 = Math.floor( Math.random() * 90  +10 );
+console.log("Enter fifth Number :" + Number5  )
+
+let Sum=Number1+Number2+Number3+Number4+Number5
+console.log("Sum of  Numbers:"+ Sum)
+let Average=((Number1+Number2+Number3+Number4+Number5)/5)
+console.log("Average of Numbers:"+ Average)
