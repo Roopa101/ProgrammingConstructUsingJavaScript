1a4307dff204a704f29fa3eff544ac33ace1b6b7
Roopa Jamadar <roopajamadar14@gmail.com>
2022-01-09 14:07:09 +0530

    [Roopa] Added: Adding Two Random Dice Number Using JavaScript

---
+++ SequencePracticeProblems/AddTwoRandomDiceNumber.js
@@ -0,0 +1,7 @@
+let DiceNumber1 = Math.floor( Math.random() * 6 )+1;
+console.log("Enter first DiceNumber :" +DiceNumber1  )
+let DiceNumber2=Math.floor(Math.random() *6)+1;
+console.log("Enter second DiceNumber:" +DiceNumber2 )
+
+let SumOfTWoDiceNumber=DiceNumber1+DiceNumber2
+console.log("Sum of Two Random Dice Number:"+ SumOfTWoDiceNumber)
