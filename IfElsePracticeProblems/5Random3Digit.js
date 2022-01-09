f4cd88ab496f3610cd6875e3523e2c51b23af321
Roopa Jamadar <roopajamadar14@gmail.com>
2022-01-09 13:43:29 +0530

    [Roopa] Added: Got Random Single Digit Number Using JavaScript

---
+++ IfElsePracticeProblems/5Random3Digit.js
@@ -0,0 +1,2 @@
+let ThreeDigitNumber=Math.floor(Math.random()* 1000)
+console.log(ThreeDigitNumber)
---
+++ SequencePracticeProblems/SingleDigitRandom.js
@@ -0,0 +1,2 @@
+let SingleDigitNumber = Math.floor(Math.random() *10)
+console.log(SingleDigitNumber)
