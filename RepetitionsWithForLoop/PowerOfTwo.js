fd7c7596bfa8842575921dbf78b0c61c3bd6e135
Roopa Jamadar <roopajamadar14@gmail.com>
2022-01-11 14:33:25 +0530

    [Roopa] Added: Printing A Table Of Powers Of TWO of a number in JavaScript

---
+++ RepetitionsWithForLoop/PowerOfTwo.js
@@ -0,0 +1,10 @@
+function powersOfTwo() {
+    const prompt = require('prompt-sync')();
+    var number = prompt("Enter the limit : ");
+    console.log("Powers of 2 are : ")
+    for (i = 0; i <= number; i++) {
+        power = Math.pow(2, i);
+        console.log(power);
+    }
+}
+powersOfTwo();
